2edfd061ad77945c4543d5ad347d72fb63c87768
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 12:30:18 +0530

    Update practical_1.1.c

--- PRACTICAL 1/practical_1.1.c
+++ PRACTICAL 1/practical_1.1.c
@@ -1,47 +1,26 @@
+//Practical 1.1
 #include <stdio.h>
 
-int main()
-
-{
+int main(){
 
    char name[20];
-
    int age;
+   long phone;
+   float percentage;
 
-   long long contact_num;
-
-   float perc;
-
-   
-
-   printf("Enter name in capital letters: ");
-
+   printf("Enter name: ");
    scanf("%s", name);
-
    printf("Enter age: ");
-
    scanf("%d", &age);
-
-   
-
    printf("Enter contact number: ");
-
-   scanf("%lld", &contact_num);
-
+   scanf("%ld", &phone);
    printf("Enter perc in 12th: ");
+   scanf("%f", &percentage);
 
-   scanf("%f", &perc);
-
-   
-
-   printf("Name= %s", name);
-
-   printf("\nAge= %d", age);
-
-   printf("\nPhone Number= %lld", contact_num);
-
-   printf("\nPerc =%.2f", perc);
+   printf("Name= %s\n", name);
+   printf("Age= %d\n", age);
+   printf("Phone Number= %ld\n", phone);
+   printf("Percentage =%.2f\n", percentage);
 
    return 0;
-
 }
