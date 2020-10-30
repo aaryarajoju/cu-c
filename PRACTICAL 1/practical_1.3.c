d1ece805176ff36eefe8789888db88d9116941db
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 14:42:17 +0530

    Create practical_1.3.c

---
+++ PRACTICAL 1/practical_1.3.c
@@ -0,0 +1,20 @@
+//PRACTICAL 1.3
+#include <stdio.h>
+
+int main(){
+    
+    int distance;
+    float timeLostByTrain, speedCar;
+    
+    printf("ENter the distance travelled from A to B: ");
+    scanf("%d", &distance);
+    
+    printf("Enter the amount of time(in mins), lost by the train while stopping at stations: ");
+    scanf("%f", &timeLostByTrain);
+    
+    speedCar = distance/(3*(timeLostByTrain/60));
+    
+    printf("The speed of car is: %f\n", speedCar);
+    
+    return 0;
+}
