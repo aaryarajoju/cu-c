178c6743b5171ee7765ef8e80f663fa5e228f46e
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 17:59:22 +0530

    Create practical_2.2.c

---
+++ PRACTICAL 2/practical_2.2.c
@@ -0,0 +1,16 @@
+//PRACTICAL 2.2
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+
+    printf("Enter the values of a and b: ");
+    scanf("%d %d",&a, &b);
+
+    printf("Output of a&b is: %d\n", a&b);
+    printf("Output of a|b is: %d\n", a|b);
+    printf("Output of a^b is: %d\n", a^b);
+
+    return 0;
+}
