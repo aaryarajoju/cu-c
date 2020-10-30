ee532dde84ce2e1fe3f83a9a3cc0e2823ec992dc
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 17:13:24 +0530

    Update practical_1.5.c

--- PRACTICAL 1/practical_1.5.c
+++ PRACTICAL 1/practical_1.5.c
@@ -3,7 +3,16 @@
 
 int main(){
 
-
+    int workA, workB, workAB;
+    
+    printf("Enter time taken(number of days) by 1st person (A) to complete the task: ");
+    scanf("%d",&workA);
+    printf("Enter time taken(number of days) by 2nd person (B) to complete the task: ");
+    scanf("%d",&workB);
+    
+    workAB = (workA * workB) / (workA + workB);
+    
+    printf("Number of days taken if both work together: %d\n", workAB);
 
     return 0;
 }
