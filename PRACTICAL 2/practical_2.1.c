cceac507d8ef9fa64e8ad4e2ac105008c67c420f
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-30 17:48:25 +0530

    Create practical_2.1.c

---
+++ PRACTICAL 2/practical_2.1.c
@@ -0,0 +1,31 @@
+//PRACTICAL 2.1
+#include <stdio.h>
+
+int main(){
+
+    int amountRam, amountMohan, amountSohan;
+    float timeRam, timeMohan, timeSohan;
+    float rateRam, rateMohan, rateSohan;
+    float interestRam, interestMohan, interestSohan;
+
+    printf("Enter Ram's loan amount, rate of interest and time: ");
+    scanf("%d %f %f", &amountRam, &rateRam, &timeRam);
+    printf("Enter Mohan's loan amount, rate of interest and time: ");
+    scanf("%d %f %f", &amountMohan, &rateMohan, &timeMohan);
+    printf("Enter SOhan's loan amount, rate of interest and time: ");
+    scanf("%d %f %f", &amountSohan, &rateSohan, &timeSohan);
+
+    interestRam = (amountRam*rateRam*timeRam)/100;
+    interestMohan = (amountMohan*rateMohan*timeMohan)/100;
+    interestSohan = (amountSohan*rateSohan*timeSohan)/100;
+
+    printf("Interest paid by Ram: %f\n", interestRam);
+    printf("Interest paid by Mohan: %f\n", interestMohan);
+    printf("Interest paid by Sohan: %f\n", interestSohan);
+
+    printf("Maximum interest is paid by - ");
+    interestRam>interestMohan?(interestRam>interestSohan?printf("Ram\n"):
+    printf("Sohan\n")):(interestMohan>interestSohan?printf("Mohan\n"):printf("Sohan\n"));
+
+    return 0;
+}
