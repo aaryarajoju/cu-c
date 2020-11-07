6ac5d4cd6a77c061bb12fe0505591723a1c0180a
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-07 15:27:28 +0530

    Update maximumOfThreeNum.c

--- PRACTICAL 2/maximumOfThreeNum.c
+++ PRACTICAL 2/maximumOfThreeNum.c
@@ -1,10 +1,10 @@
-// Example : Maximum of Three numbers, using ternary conditions
+// Example : Maximum of Three numbers, using ternary conditional operator
 
 #include <stdio.h>
 
 int main (){
 
-    int num1, num2, num3, max;
+    int num1, num2, num3;
 
     printf("Enter three numbers: ");
     scanf("%d %d %d", &num1, &num2, &num3);
