bb7d86d6b9badbcc3bc54c826353e34cb6e29595
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-11 11:05:55 +0530

    edited 3.1

--- PRACTICAL 3/practical_3.1.c
+++ PRACTICAL 3/practical_3.1.c
@@ -3,26 +3,34 @@
 
 int main (){
     
+    //initiating the variables
     float classSize, numOfGirls, numOfBoys, x, y;
     float percBoys, percGirls;
     int diff;
 
+
+    //user-input for class size
     printf("Enter the Class Size: ");
     scanf("%f", &classSize);
 
+    //user input for the ratio of girls to boys
     printf("Enter the ratio of number of girls to the number of boys in the class: ");
     scanf("%f:%f", &x, &y);
 
+    //calculating the number of students
     numOfGirls = (x / (x + y)) * classSize;
     numOfBoys = (y / (x + y)) * classSize;
 
+    //difference between the number of students
     diff = numOfGirls - numOfBoys;
 
+    //percentages of the students
     percBoys = numOfBoys / classSize * 100;
     percGirls = numOfGirls / classSize * 100;
 
     printf("\n");
 
+    //printing the conclusion
     if (diff > -5 && diff < 5){
         printf("Equal Oppotunities of education are there for both boys and girls\n\n");
     } else if (percBoys > 70){
