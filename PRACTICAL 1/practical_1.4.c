673c049c2a0448d91991aedcd652444e14ddf8c0
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-11 18:52:52 +0530

    added comments

--- PRACTICAL 1/practical_1.4.c
+++ PRACTICAL 1/practical_1.4.c
@@ -1,18 +1,23 @@
 //PRACTICAL 1.4
+//How many students are there in the class?
+
 #include <stdio.h>
 
 int main(){
 
+    //initializing the variables
     int positionFromTop, positionFromBottom, numOfStudents;
     
+    //asking the user for inputting the position of sonu from the top
     printf("Enter the position of Sonu from Top: ");
     scanf("%d", &positionFromTop);
 
+    //asking the user for inputting the position of sonu from bottom
     printf("Enter the position of Sonu from Bottom: ");
     scanf("%d", &positionFromBottom);
     
+    //calculating the total number of students and printing it
     numOfStudents = positionFromTop + positionFromBottom - 1;
-    
     printf("The number of students in the class is: %d\n", numOfStudents);
 
     return 0;
