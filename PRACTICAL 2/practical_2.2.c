cb0b444cab63cf5893e0141c7d1c39c93396f991
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-11 14:57:59 +0530

    added comments

--- PRACTICAL 2/practical_2.2.c
+++ PRACTICAL 2/practical_2.2.c
@@ -1,13 +1,18 @@
 //PRACTICAL 2.2
+//Write a program to input two integers from user by using single scanf. Compute and display the value for a& b, a| b, a f b.
+
 #include <stdio.h>
 
 int main(){
 
+    //initializing two variables
     int a, b;
 
+    //asking the user to input two numbers
     printf("Enter the values of a and b: ");
     scanf("%d %d",&a, &b);
 
+    //printing out the resutls of using the bit wise operators
     printf("Output of a&b is: %d\n", a&b);
     printf("Output of a|b is: %d\n", a|b);
     printf("Output of a^b is: %d\n", a^b);
