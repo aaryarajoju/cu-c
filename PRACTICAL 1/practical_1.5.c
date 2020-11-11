4a3094162485f9cedd5a00d9ca2836f7368b256b
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-11 18:52:58 +0530

    added comments

--- PRACTICAL 1/practical_1.5.c
+++ PRACTICAL 1/practical_1.5.c
@@ -1,17 +1,21 @@
 //PRACTICAL 1.5
+//Write a program to calculate and print the number of days to be taken to complete the work if A and B work together
+
 #include <stdio.h>
 
 int main(){
 
+    //initializing the variables
     int workA, workB, workAB;
     
+    //asking the user to input the number of days taken by each of the person to do the task alsone
     printf("Enter time taken(number of days) by 1st person (A) to complete the task: ");
     scanf("%d",&workA);
     printf("Enter time taken(number of days) by 2nd person (B) to complete the task: ");
     scanf("%d",&workB);
     
+    //calculating the number of days it would take for both of them to do the work, and printing it
     workAB = (workA * workB) / (workA + workB);
-    
     printf("Number of days taken if both work together: %d\n", workAB);
 
     return 0;
