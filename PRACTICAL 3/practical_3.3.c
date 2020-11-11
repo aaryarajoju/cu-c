67c5cd375694e297f9010011caa6a55db1e53489
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-11 14:05:25 +0530

    added the remaining two programs for practical 3

---
+++ PRACTICAL 3/practical_3.3.c
@@ -0,0 +1,9 @@
+//PRACTICAL 3.3
+#include <stdio.h>
+
+int main (){
+    
+    
+
+    return 0;
+}
---
+++ PRACTICAL 3/practical_3.4.c
@@ -0,0 +1,9 @@
+//PRACTICAL 3.4
+#include <stdio.h>
+
+int main (){
+    
+    
+
+    return 0;
+}
