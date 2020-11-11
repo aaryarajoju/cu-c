3e875b04a35025cc28826874283e12a70ebad897
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-12 02:03:58 +0530

    completed pass or fail

--- PRACTICAL 3/gotoPassOrFail.c
+++ PRACTICAL 3/gotoPassOrFail.c
@@ -4,17 +4,21 @@
 
 int main (){
 
+    //initializing the variable
     int marks;
 
+    //getting the user input
     printf("Enter the marks: ");
     scanf("%d", &marks);
 
+    //checking if the marks are above the given threshold or not
     if (marks >= 75){
         goto pass;
     } else {
         goto fail;
     }
 
+    //printing pass or fail, depending on the marks
     pass:
         printf("Congratulations, you've passed!\n");
         goto end;
