a558f222ddb2fafa1233538836b81b121a626bf6
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-11 18:52:35 +0530

    added comments

--- PRACTICAL 1/practical_1.1.c
+++ PRACTICAL 1/practical_1.1.c
@@ -1,13 +1,17 @@
-//Practical 1.1
+//PRACTICAL 1.1
+//Write a program to input details of a under-graduate student
+
 #include <stdio.h>
 
 int main(){
 
+   //initializing the variables
    char name[20];
    int age;
    long phone;
    float percentage;
 
+   //asking the user for user input about the student details
    printf("Enter name: ");
    scanf("%s", name);
    printf("Enter age: ");
@@ -17,6 +21,7 @@ int main(){
    printf("Enter perc in 12th: ");
    scanf("%f", &percentage);
 
+   //printing the detail entered
    printf("Name= %s\n", name);
    printf("Age= %d\n", age);
    printf("Phone Number= %ld\n", phone);
