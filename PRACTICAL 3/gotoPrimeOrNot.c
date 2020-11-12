f4b09382754dcd1a9b5f35748e2efa677bd8a8f7
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-12 18:50:20 +0530

    added comments to goto prime

--- PRACTICAL 3/gotoPrimeOrNot.c
+++ PRACTICAL 3/gotoPrimeOrNot.c
@@ -4,20 +4,24 @@
 
 int main (){
     
+    //initializing the variables
     int num;
     int i = 2;
     int lcd;
 
+    //asking the user input for the number to be checked
     printf("Enter a number: ");
     scanf("%d", &num);
 
+    //if case for narrowing the search, i.e. if the num is 2, then it's prime, if it's an even number other than 2, then it's not prime
     if (num == 2){
         printf("The number 2 is prime\n\n");
     } else if (num % 2 == 0){
         printf("The number %d is not a prime number\n\n", num);
     } else {
 
-
+        //checking if an odd number is prime or not
+        
         again:
 
         if (num % i == 0){
