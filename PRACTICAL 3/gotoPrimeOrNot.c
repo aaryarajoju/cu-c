316f7fb6cd3a77190652969a5c2fd80f98152b57
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-12 17:32:29 +0530

    draft for goto prime

--- PRACTICAL 3/gotoPrimeOrNot.c
+++ PRACTICAL 3/gotoPrimeOrNot.c
@@ -1,9 +1,42 @@
-// EXAMPLE: printing is a number is prime or not, using goto
+// EXAMPLE: checking if a number is prime or not, using goto
+
 #include <stdio.h>
 
 int main (){
     
-    
+    int num;
+    int i = 3;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 2){
+        printf("The number 2 is prime\n\n");
+    } else if (num % 2 == 0){
+        printf("The number %d is not a prime number\n\n", num);
+    } else {
+
+        
+
+    }
 
     return 0;
 }
+
+
+/*
+int isPrime(long n) {
+    if (n==2){
+        return 1;
+    } else if (n%2==0){
+        return 2;
+    } else {
+        for (int i = 3; i < (int)n; i++) {
+            if ((int)n%i==0){
+                return (int)i;
+            }
+        } 
+    }
+    return 1;
+}
+*/
