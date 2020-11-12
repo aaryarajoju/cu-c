0c5afcec05ae0bb459643590c9ff0f81f180513a
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-12 19:04:17 +0530

    added comments to goto sum

--- PRACTICAL 3/gotoSum.c
+++ PRACTICAL 3/gotoSum.c
@@ -4,14 +4,17 @@
 
 int main (){
     
+    //initializing the variables
     int num; 
     int sum = 0;
     int i = 1;
 
+
+    //asking the user for inputting a number
     printf("Enter a number: ");
     scanf("%d", &num);
 
-
+    //calculating the sum
     again:
 
     sum = sum + i;
@@ -21,7 +24,7 @@ int main (){
         goto again;
     }
 
-
+    //printing the sum
     printf("The sum of first %d numbers is %d\n\n", num, sum);
 
     return 0;
