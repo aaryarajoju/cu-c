f38dd6062e87a9cb7aed53d08057409257a0420c
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-13 01:11:06 +0530

    edited comments in goto palindrome

--- PRACTICAL 3/gotoPalindrome.c
+++ PRACTICAL 3/gotoPalindrome.c
@@ -35,16 +35,3 @@ int main (){
 
     return 0;
 }
-
-
-/*
-int n, rev = 0, remainder;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    while (n != 0) {
-        remainder = n % 10;
-        rev = rev * 10 + remainder;
-        n /= 10;
-    }
-    printf("Reversed number = %d", rev);
-*/
