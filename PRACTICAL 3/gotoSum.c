40910ee355192bf0c463428e33410b13d70883ae
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-12 17:33:02 +0530

    draft for goto sum

--- PRACTICAL 3/gotoSum.c
+++ PRACTICAL 3/gotoSum.c
@@ -1,9 +1,25 @@
 // EXAMPLE: printing the sum of first N natural numbers, using goto
+
 #include <stdio.h>
 
 int main (){
     
-    
+    int num; 
+    int sum = 0;
+    int i = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    again:
+    sum = sum + i;
+    i++;
+
+    if (i < num){
+        goto again;
+    }
+
+    printf("The sum of first %d numbers is %d\n\n", num, sum);
 
     return 0;
 }
