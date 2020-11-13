e3ba9a52eeb75455047723f8b7200479e3570a39
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-13 14:47:12 +0530

    draft for 3.3

--- PRACTICAL 3/practical_3.3.c
+++ PRACTICAL 3/practical_3.3.c
@@ -6,7 +6,70 @@ Total money they have initially is Rs. N. After spending money in the market who
 
 int main (){
     
-    
+    float initialRatioAmba, initialRatioAmbika, initialRatioAmbalika;
+    float initialAmba, initialAmbika, initialAmbalika;
+    float totalMoney;
+    float spentRatioAmba, spentRatioAmbika, spentRatioAmbalika;
+    float spentAmba, spentAmbika, spentAmbalika;
+    float savedRatioAmba, savedRatioAmbika, savedRatioAmbalika;
+    float savedAmba, savedAmbika, savedAmbalika;
+    float x, y, z;
+
+    printf("Enter the total amount of money that they had before spending : ");
+    scanf("%f", &totalMoney);
+
+    printf("Enter the ratio of money that they had (in the format x:y:z) : ");
+    scanf("%f:%f:%f", &initialRatioAmba, &initialRatioAmbika, &initialRatioAmbalika);
+
+    printf("Enter the ratio of money that they spent (in the format p:q:r) : ");
+    scanf("%f:%f:%f", &spentRatioAmba, &spentRatioAmbika, &spentRatioAmbalika);
+   
+    printf("Enter the ratio of money that they saved (in the format g:h:i) : ");
+    scanf("%f:%f:%f", &savedRatioAmba, &savedRatioAmbika, &savedRatioAmbalika); 
+
+
+    x = totalMoney / (initialAmba + initialAmbika + initialAmbalika);
+
+    initialAmba = x * initialRatioAmba;
+    initialAmbika = x * initialRatioAmbika;
+    initialAmbalika = x * initialRatioAmbalika;
+
+
+    if ((spentRatioAmba/spentRatioAmba) != (savedRatioAmba/savedRatioAmbika)){
+
+        //calculation:
+
+
+
+
+
+        spentAmba = y * spentRatioAmba;
+        spentAmbika = y * spentRatioAmbika;
+        spentAmbalika = y * spentRatioAmbalika;
+
+        savedAmba = z * savedRatioAmba;
+        savedAmbika = z * savedRatioAmbika;
+        savedAmbalika = z * savedRatioAmbalika;
+
+        if (savedAmbika > savedAmba && savedAmbika > savedAmbalika){
+            printf("\n Ambika left with the maximum amount ($ %d) \n\n", (int)savedAmbika);
+        } else if (savedAmba > savedAmbalika){
+            printf("\n Amba left with the maximum amount ($ %d) \n\n", (int)savedAmba);
+        } else {
+            printf("\n Ambalika left with the maximum amount ($ %d) \n\n", (int)savedAmbalika);
+        }
+    } else if ((spentRatioAmba/spentRatioAmbika) == (savedRatioAmba/savedRatioAmbika)){
+        
+        if ((spentRatioAmba/spentRatioAmbika) != (initialAmba/initialAmbika)){
+            printf("\n INVALID! No valid solution exists \n\n");
+        } else if ((spentRatioAmba/spentRatioAmbika) == (initialAmba/initialAmbika)){
+            printf("\n All of them have the same amount of money left \n\n");
+        } else {
+            printf("\n INVALID! \n\n");
+        }       
+    } else {
+        printf("\n INVALID! \n\n");
+    } 
 
     return 0;
 }
