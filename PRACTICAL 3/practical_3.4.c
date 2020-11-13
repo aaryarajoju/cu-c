e4fb636cec9b17c5bad8a1deda923c37b1d837d9
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-13 09:17:43 +0530

    draft for 3.4

--- PRACTICAL 3/practical_3.4.c
+++ PRACTICAL 3/practical_3.4.c
@@ -1,4 +1,8 @@
 //PRACTICAL 3.4
+/*While travelling in a train, you observe some college students pulling the alarm chain simply to get down at their desired point. 
+Out of n students m<=n times students pull the chain 
+WAP to print if any action needs to be taken if m out of n students pull the chain in the train*/
+
 #include <stdio.h>
 
 int main (){
