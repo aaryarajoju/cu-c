588d3b23b4161ad01016bb30101035cbe4ccdab0
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-13 09:17:52 +0530

    draft for 3.3

--- PRACTICAL 3/practical_3.3.c
+++ PRACTICAL 3/practical_3.3.c
@@ -1,4 +1,7 @@
 //PRACTICAL 3.3
+/*Amba, Aambika and Ambalika have money in the ratio x:y:z. All go to market and spend money in ratio p:q:r. 
+Total money they have initially is Rs. N. After spending money in the market who has maximum amount left with?*/
+
 #include <stdio.h>
 
 int main (){
