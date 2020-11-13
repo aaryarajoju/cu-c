33b973e9df76b20387bd919a22bbdde5a52f1a88
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-13 17:06:39 +0530

    completed 3.4

--- PRACTICAL 3/practical_3.4.c
+++ PRACTICAL 3/practical_3.4.c
@@ -7,6 +7,34 @@ WAP to print if any action needs to be taken if m out of n students pull the cha
 
 int main (){
     
+    float numOfStudents, numOfStudentsPullingChain;
+    float perc;
+
+    printf("\nEnter the total number of students: ");
+    scanf("%d", &numOfStudents);
+
+    printf("Enter the number of students pulling the chain: ");
+    scanf("%d", &numOfStudentsPullingChain);
+
+    if (numOfStudentsPullingChain <= numOfStudents){
+        
+        perc = (numOfStudentsPullingChain / numOfStudents) * 100;
+
+        if (perc >= 80){
+            printf("\nStrict Action Required\n\n");
+        } else if (perc >= 50){
+            printf("\nGuidlines to be issued\n\n");
+        } else if (perc >= 10){
+            printf("\nRestrict the action\n\n");
+        } else if (perc < 10){
+            printf("\nNo action required\n\n");
+        } else {
+            printf("\nINVALID!\n\n");
+        }
+
+    } else {
+        printf("\nINVALID! The number of students pulling the chain cannot be more than the total number of students\n\n");
+    }
     
 
     return 0;
