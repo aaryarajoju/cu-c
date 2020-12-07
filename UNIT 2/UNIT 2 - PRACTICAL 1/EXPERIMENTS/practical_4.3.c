8928f6d4f29ee3cd209d6235f11085b07c946aba
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-12-07 13:52:17 +0530

    added 4.3

--- UNIT 2/UNIT 2 - PRACTICAL 1/EXPERIMENTS/practical_4.3.c
+++ UNIT 2/UNIT 2 - PRACTICAL 1/EXPERIMENTS/practical_4.3.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(){
+   int n,i;
 
+   printf("Enter no of customer : ");
+   scanf("%d",&n);
 
-    return 0;
+   float principal[n],time[n],r,interest=0;
+
+   printf("Enter annual rate of interest : ");
+   scanf("%f",&r);
+
+   printf("\n********Enter customer data********\n");
+
+   for(i=0;i<n;i++){
+       printf("\nCustomer %d\n",i+1);
+
+       printf("Principal amount : ");
+       scanf("%f",&principal[i]);
+
+       printf("Time period : ");
+       scanf("%f",&time[i]);
+   }
+
+   for(i=0;i<n;i++){
+       interest+=principal[i]*(pow((1+(r/(100))),time[i])-1);
+   }
+
+   printf("\n\nTotal interest earned by bank from all customer : %.2f",interest);
+
+   return 0;
 }
