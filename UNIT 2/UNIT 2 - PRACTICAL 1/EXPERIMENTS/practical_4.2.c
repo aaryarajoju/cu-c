c065925803924a99567d999163ef02ae0dfeeb9d
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-12-07 13:52:10 +0530

    added 4.2

--- UNIT 2/UNIT 2 - PRACTICAL 1/EXPERIMENTS/practical_4.2.c
+++ UNIT 2/UNIT 2 - PRACTICAL 1/EXPERIMENTS/practical_4.2.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main(){
+   int p,i;
 
+   printf("Enter no : ");
+   scanf("%d",&p);
 
-    return 0;
+   if(p>999999) printf("Not possible");
+
+   else
+       for(i=999999;;i--){
+           if(i%p==0){
+               printf("%d is 6-digit greatest no divisible by %d\n\n",i,p);
+               printf("%d is 7-digit smallest no divisible by %d\n\n",i+p,p);
+               break;
+           }
+       }
+
+   return 0;
 }
