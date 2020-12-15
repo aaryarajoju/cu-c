ed0453e23ea7ed61f68021fb608cd723f2469279
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-15 23:28:24 +0530

    added 5.4

--- UNIT 2/UNIT 2 - PRACTICAL 2/EXPERIMENTS/practical_5.4.c
+++ UNIT 2/UNIT 2 - PRACTICAL 2/EXPERIMENTS/practical_5.4.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int n;
 
+    printf("Enter size of array : ");
+    scanf("%d",&n);
 
+    int a[n],temp;
+
+    printf("Enter %d numbers...",n);
+
+    for(int i=0;i<n;i++) {
+        printf("\n%d : ",i+1);
+        scanf("%d",&a[i]);
+    }
+    
+    for(int i=n-5;i<n;i++) {
+        for(int j=i;j>0;j--) {
+            temp=a[j];
+            a[j]=a[j-1];
+            a[j-1]=temp;
+        }
+    }
+
+    puts("Modified series...");
+
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+    
     return 0;
 }
