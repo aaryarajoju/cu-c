449c6282ddd6141364397031b80ea502491bb93c
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-15 23:27:51 +0530

    added 5.1

--- UNIT 2/UNIT 2 - PRACTICAL 2/EXPERIMENTS/practical_5.1.c
+++ UNIT 2/UNIT 2 - PRACTICAL 2/EXPERIMENTS/practical_5.1.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
+int main() {
 
+    int n;
+
+    printf("Enter no of persons : ");
+    scanf("%d",&n);
+
+    float amount[n],p,q,r;
+
+    puts("Enter amount of each person...");
+
+    for(int i=0;i<n;i++) {
+        printf("%d : ",i+1);
+        scanf("%f",&amount[i]);
+    }
+
+    for(r=1;r<=amount[0];r++)
+        for(q=1;q<=amount[0];q++)
+            for(p=1;p<=amount[0];p++)
+                if((r*1+q*0.50+p*0.25)==amount[0])
+                    goto A;
+
+    A:
+    printf("Person 1 Rs 50 paise 25 paise\n");
+
+    for(int i=0;i<n;i++) {
+    printf("%d %11.0f %7.0f %11.0f \n",i+1,r*(amount[i]/amount[0]),q*(amount[i]/amount[0]),p*(amount[i]/amount[0]));
+    }
 
     return 0;
 }
