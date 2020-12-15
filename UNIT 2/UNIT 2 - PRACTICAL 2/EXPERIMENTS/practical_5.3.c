877c9a6faea0e3b6152e4b828956fa1666bf012d
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-15 23:28:15 +0530

    added 5.3

--- UNIT 2/UNIT 2 - PRACTICAL 2/EXPERIMENTS/practical_5.3.c
+++ UNIT 2/UNIT 2 - PRACTICAL 2/EXPERIMENTS/practical_5.3.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int n;
 
+    printf(“Enter no of goods: “);
+    scanf("%d", &n);
+
+    float cost[n],x,y,p,total=0;
+
+    printf("Enter cost of %d goods\n",n);
+
+    for(int i=0;i<n;i++) {
+        printf("%d : ",i+1);
+        scanf("%f",&cost[i]);
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            if(cost[j]<cost[i]) {
+                cost[j]+=cost[i];
+                cost[i]=cost[j]-cost[i];
+                cost[j]-=cost[i];
+            }
+        }
+    }
+
+    printf("Loss on most expensive thing : ");
+    scanf("%f",&x);
+
+    printf("Gain on cheapest thing : ");
+    scanf("%f",&y);
+
+    if(n>2) {
+        printf("Gain on rest of the thing : "); scanf("%f",&p);
+    }
+
+    for(int i=0;i<n;i++) {
+        if(cost[n-1]==cost[i])
+            total+=(y*cost[i])/100;
+        else if(cost[0]==cost[i])
+            total-=(x*cost[i])/100;
+        else
+            total+=(p*cost[i])/100;
+    }         
+
+    if(total<0)
+        printf("Rs. %.2f loss on goods",total*-1);
+    else if(total>0)
+        printf("Rs. %.2f gain on goods",total);
+    else
+        printf("Neither gain nor loss on goods");
 
     return 0;
 }
