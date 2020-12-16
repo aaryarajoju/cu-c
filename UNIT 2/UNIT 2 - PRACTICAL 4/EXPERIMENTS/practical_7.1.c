4228736c4d95d4625b0d47ae7a8ba643f830a947
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-16 20:36:00 +0530

    completed 7.1

--- UNIT 2/UNIT 2 - PRACTICAL 4/EXPERIMENTS/practical_7.1.c
+++ UNIT 2/UNIT 2 - PRACTICAL 4/EXPERIMENTS/practical_7.1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(){
+int max_of_four(float a, float b, float c, float d) {
 
+    return (int) fmax(fmax(fmax(a, b), c), d);
+}
+
+int main() {
+
+    float a,b,c,d;
+
+    printf("Enter any 4 numbers: ");
+    scanf("%f, %f, %f, %f", &a, &b, &c, &d);
+
+    printf("Largest out of 4 no is : %d\n\n", max_of_four(a,b,c,d));
 
     return 0;
 }
