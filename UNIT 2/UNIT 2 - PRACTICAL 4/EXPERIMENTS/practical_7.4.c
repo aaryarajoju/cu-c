92d5d1f132353333aad8eccc193121d128f1f5f6
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-19 15:12:56 +0530

    added 7.4

--- UNIT 2/UNIT 2 - PRACTICAL 4/EXPERIMENTS/practical_7.4.c
+++ UNIT 2/UNIT 2 - PRACTICAL 4/EXPERIMENTS/practical_7.4.c
@@ -1,7 +1,50 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
+float avg_ht(int age[],int n) {
 
+    int h[n],i;
+
+    float avg,sum=0.0;
+
+    for(i=0;i<n;i++) {
+
+        if(age[i]==16)
+            h[i]=168;
+
+        else if(age[i]==17)
+            h[i]=170;
+
+        else if(age[i]==18)
+            h[i]=172;
+
+        else if(age[i]==19)
+            h[i]=175;
+
+        sum=sum+h[i];
+
+        avg=sum/n;
+    }
+
+    return avg;
+}
+
+int main() {
+
+    int age[30],n,i;
+    float ah;
+
+    printf("Enter no. of students in the class:");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++) {
+        printf("\nEnter age of student %d:",i+1);
+        scanf("%d",&age[i]);
+    }
+
+    ah=avg_ht(age,n);
+
+    printf("Average height of class is:%f",ah);
 
     return 0;
 }
+
