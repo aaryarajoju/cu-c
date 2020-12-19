8b95b3d029431da61d8449bff57070d75e347ba2
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-19 15:12:35 +0530

    added 7.2

--- UNIT 2/UNIT 2 - PRACTICAL 4/EXPERIMENTS/practical_7.2.c
+++ UNIT 2/UNIT 2 - PRACTICAL 4/EXPERIMENTS/practical_7.2.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h>
 
-int main(){
+int factorial(int n) {
+    if(n==0) 
+        return 1;
 
+    else
+        return n*factorial(n-1);
+}
+
+int main() {
+
+    int n;
+
+    printf("Enter no to find factorial : "); scanf("%d", &n);
+
+    if (n >= 0)
+        printf("Factorial of given no %d is : %d", n, factorial(n));
+
+    else
+        printf("Not Possible");
 
     return 0;
 }
