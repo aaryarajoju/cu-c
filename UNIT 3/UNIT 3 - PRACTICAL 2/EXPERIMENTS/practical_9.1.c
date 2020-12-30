275f0e25e8ba359599a4b492dcba8eba1ab25161
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-30 14:51:36 +0530

    ADDED 9.1

---
+++ UNIT 3/UNIT 3 - PRACTICAL 2/EXPERIMENTS/practical_9.1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int i=0, n=1;
+
+struct Patient{
+   char name[10];
+   int patient_id;
+   char group[2];
+   long long int contact;
+   char disease[10];
+   int DOA;
+}patient[5];
+
+
+void AddPatient(){
+
+   for(i=0; i<n; i++){
+
+       printf("\t Patient name =");
+       scanf("%s", patient[i].name);
+
+       printf("\t Patient ID =");
+       scanf("%d", &patient[i].patient_id);
+
+       printf("\t Blood Group =");
+       scanf("%s", patient[i].group);
+
+       printf("\t Contact =");
+       scanf("%lld", &patient[i].contact);
+
+       printf("\t Disease =");
+       scanf("%s", patient[i].disease);
+
+       printf("\t Date of Admission =");
+       scanf("%d", &patient[i].DOA);
+
+   }
+}
+
+void PrintPatient(){
+
+   printf("Patient Record:\n\n");
+
+   for(i=0; i<n; i++){
+
+       printf("\t Patient name = %s\n", patient[i].name);
+       printf("\t Patient ID = %d\n", patient[i].patient_id);
+       printf("\t Blood Group = %s\n", patient[i].group);
+       printf("\t Contact = %lld\n", patient[i].contact);
+       printf("\t Disease = %s\n", patient[i].disease);
+       printf("\t Date of Admission = %d\n", patient[i].DOA);
+   }
+}
+
+
+
+int main(){
+
+   AddPatient();
+   PrintPatient();
+   return 0;
+
+}
