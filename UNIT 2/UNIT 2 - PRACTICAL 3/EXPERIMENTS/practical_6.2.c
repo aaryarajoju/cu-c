bea63e4e3ccfbed24e6fd549f9cf8d71b0b08725
AARYA RAJOJU <aaryarajoju@gmail.com>
2021-01-06 16:36:51 +0530

    add 6.2

--- UNIT 2/UNIT 2 - PRACTICAL 3/EXPERIMENTS/practical_6.2.c
+++ UNIT 2/UNIT 2 - PRACTICAL 3/EXPERIMENTS/practical_6.2.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+int main() {
 
+    int l;
+    char str[20]; printf("Enter string : \n");
+
+    scanf("%[^\n]%*c", str);
+
+    l=strlen(str);
+
+    for(int i=1; i<=l; i++) {
+
+        for(int j=0,k=0;k!=l; j++) {
+
+            for(k=j; k<(j+i); k++) {
+                printf("%c", str[k]);
+            }
+
+            printf(",");
+
+        }
+    }
 
     return 0;
 }
