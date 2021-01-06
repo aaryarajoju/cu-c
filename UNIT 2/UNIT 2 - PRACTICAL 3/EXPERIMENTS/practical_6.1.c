ea94f530bfc1c6af195fba2e5f8f7684b27beda3
AARYA RAJOJU <aaryarajoju@gmail.com>
2021-01-06 16:36:45 +0530

    add 6.1

--- UNIT 2/UNIT 2 - PRACTICAL 3/EXPERIMENTS/practical_6.1.c
+++ UNIT 2/UNIT 2 - PRACTICAL 3/EXPERIMENTS/practical_6.1.c
@@ -1,7 +1,50 @@
-#include <stdio.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(){
+int main() {
 
+    char str[50];
+    int i, l, d=0, lc=0, uc=0,s=0, count=0;
+
+    printf("Enter string : \n");
+    scanf("%[^\n]s",str);
+
+    l=strlen(str);
+
+    for(i=0;i<l;i++) {
+
+        if(!isdigit(str[i]))
+            d+=1;
+
+        if(!islower(str[i]))
+            lc+=1;
+
+        if(!isupper(str[i]))
+            uc+=1;
+
+        if(!(str[i]=='!' || str[i]=='@' || str[i]=='#' || str[i]=='$' || str[i]=='%' || str[i]=='^' || str[i]=='&' || str[i]=='*' || str[i]=='(' || str[i]==')' || str[i]=='-' || str[i]=='+'))
+            s+=1;
+    }
+
+    if(d==l)
+        count+=1;
+
+    if(lc==l)
+        count+=1;
+
+    if(uc==l)
+        count+=1;
+
+    if(s==l)
+        count+=1;
+
+    if(count>6-l)
+        printf("Password is strong with %d number of characters", l);
+
+    else
+        printf("Number of characters needs to be added are: %d",6-l);
 
     return 0;
 }
